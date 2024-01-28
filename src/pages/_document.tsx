9ea309e487a3050b3e41188971d82ec9c550b8d8
Nagy László Levente <nagylacika2002@gmail.com>
2024-01-28 17:03:54 +0100

    YPM-44: Simple navbar implemented

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {Html, Head, Main, NextScript} from 'next/document'
+import Navbar from "@/components/layout/Navbar";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head/>
+            <Navbar/>
+            <body>
+                <Main/>
+                <NextScript/>
+            </body>
+        </Html>
+    )
 }
